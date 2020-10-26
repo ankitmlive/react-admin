e275306322d07a8018b8acc5cf24c8e78fcdfc89
ankitmlive <ankitmlive@gmail.com>
2020-10-26 10:37:54 +0530

    added cong

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Router from "./router/Router"
-//import "./components/@vuexy/rippleButton/RippleButton"
+import "./components/@vuexy/rippleButton/RippleButton"
 import "react-perfect-scrollbar/dist/css/styles.css"
 import "prismjs/themes/prism-tomorrow.css"
 
