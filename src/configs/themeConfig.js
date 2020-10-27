c1d2a282dc64021a03513d8096d33ddb71f71613
ankitmlive <ankitmlive@gmail.com>
2020-10-27 15:39:09 +0530

    tweak

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -10,8 +10,8 @@ const themeConfig = {
     footerType: "static", // options[String]: static(default) / sticky / hidden
     disableCustomizer: true, // options[Boolean]: true, false(default)
     hideScrollToTop: false, // options[Boolean]: true, false(default)
-    disableThemeTour: false, // options[Boolean]: true, false(default)
-    menuTheme: "primary", // options[String]: primary / success / danger / info / warning / dark
+    disableThemeTour: true, // options[Boolean]: true, false(default)
+    menuTheme: "dark", // options[String]: primary / success / danger / info / warning / dark
     direction: "ltr" // options[String] : ltr / rtl
   };
   
