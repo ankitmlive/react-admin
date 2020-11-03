c0e1b62a756336797d38e8f211de0af6f7fe374a
ankitmlive <ankitmlive@gmail.com>
2020-11-03 16:22:35 +0530

    theme changed

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -3,7 +3,7 @@
 
 const themeConfig = {
     layout: "vertical", // options[String]: "vertical"(default), "horizontal"
-    theme: "light", // options[String]: 'light'(default), 'dark', 'semi-dark'
+    theme: "semi-dark", // options[String]: 'light'(default), 'dark', 'semi-dark'
     sidebarCollapsed: true, // options[Boolean]: true, false(default)
     navbarColor: "default", // options[String]: default / primary / success / danger / info / warning / dark
     navbarType: "sticky", // options[String]: floating(default) / static / sticky / hidden
