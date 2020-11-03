b1ae4daef7cb2d1d739cb7e501cd86e0162dd433
ankitmlive <ankitmlive@gmail.com>
2020-11-03 17:15:58 +0530

    tweak

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -4,7 +4,7 @@
 const themeConfig = {
     layout: "vertical", // options[String]: "vertical"(default), "horizontal"
     theme: "semi-dark", // options[String]: 'light'(default), 'dark', 'semi-dark'
-    sidebarCollapsed: true, // options[Boolean]: true, false(default)
+    sidebarCollapsed: false, // options[Boolean]: true, false(default)
     navbarColor: "default", // options[String]: default / primary / success / danger / info / warning / dark
     navbarType: "sticky", // options[String]: floating(default) / static / sticky / hidden
     footerType: "static", // options[String]: static(default) / sticky / hidden
