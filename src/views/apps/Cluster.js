93abf4bafa2e00e4f914d7d360374c9583e439b5
ankitmlive <ankitmlive@gmail.com>
2020-11-04 17:45:44 +0530

    cluster

--- src/views/apps/Cluster.js
+++ src/views/apps/Cluster.js
@@ -1,6 +1,7 @@
 import React from "react"
-import axios from "axios"
 import {Card, CardHeader, CardTitle, Table} from "reactstrap"
+//import axios from "axios"
+const axios = require('axios');
 
 class Cluster extends React.Component {
 
@@ -10,21 +11,28 @@ class Cluster extends React.Component {
     errors: null
   };
 
-  // Now we're going to make a request for data using axios
-  getCluster() {
-    axios
-      .get("https://cp.georadius.in/console_result.php?&action=search_cluster&cluster_name=&cluster_location=&rand=539798834")
-      // Once we get a response and store data, let's change the loading state
-      .then(response => {
-        console.log(response)
-        this.setState({
-          clusterLists: response.data.posts,
-          isLoading: false
-        });
-      })
-      // If we catch any errors connecting, let's update accordingly
-      .catch(error => this.setState({ error, isLoading: false }));
+  async getCluster() {
+  const result = await axios.get(
+    "https://api.coindesk.com/v1/bpi/currentprice.json"
+  );
+  console.log(result)
   }
+
+  // Now we're going to make a request for data using axios
+  // getCluster() {
+  //   axios
+  //     .get("https://cp.georadius.in/console_result.php?&action=search_cluster&cluster_name=&cluster_location=&rand=539798834")
+  //     // Once we get a response and store data, let's change the loading state
+  //     .then(response => {
+  //       console.log(response)
+  //       this.setState({
+  //         clusterLists: response.data.posts,
+  //         isLoading: false
+  //       });
+  //     })
+  //     // If we catch any errors connecting, let's update accordingly
+  //     .catch(error => this.setState({ error, isLoading: false }));
+  // }
 
   componentDidMount() {
     this.getCluster();
