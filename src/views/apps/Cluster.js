37914529d802665d7869b7f5e91b422913a46b2e
ankitmlive <ankitmlive@gmail.com>
2020-11-04 14:33:21 +0530

    creating cluster component

--- src/views/apps/Cluster.js
+++ src/views/apps/Cluster.js
@@ -1,21 +1,69 @@
 import React from "react"
-import { Row, Col } from "reactstrap"
-//import DomainLists from "./DomainList"
-//import "../../assets/scss/pages/dashboard-analytics.scss"
+import axios from "axios"
+import {Card, CardHeader, CardTitle, Table} from "reactstrap"
 
 class Cluster extends React.Component {
+
+  state = {
+    clusterLists: [],
+    isLoading: true,
+    errors: null
+  };
+
+  // Now we're going to make a request for data using axios
+  getCluster() {
+    axios
+      .get("https://cp.georadius.in/console_result.php?&action=search_cluster&cluster_name=&cluster_location=&rand=539798834")
+      // Once we get a response and store data, let's change the loading state
+      .then(response => {
+        console.log(response)
+        this.setState({
+          clusterLists: response.data.posts,
+          isLoading: false
+        });
+      })
+      // If we catch any errors connecting, let's update accordingly
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  componentDidMount() {
+    this.getCluster();
+  }
+
   render() {
+    const { isLoading, clusterLists } = this.state;
     return (
-      <React.Fragment>
-        <Row>
-          <Col sm="12">
-            {/* <DomainLists /> */}
-            <h1>Hello Cluster</h1>
-          </Col>
-        </Row>
-      </React.Fragment>
+      <Card>
+        <CardHeader>
+          <CardTitle>Cluster</CardTitle>
+        </CardHeader>
+        <Table
+          responsive
+          className="dashboard-table table-hover-animation mb-0 mt-1"
+        >
+          <thead>
+            <tr>
+              <th>Cluster Name</th>
+              <th>Cluster Location</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clusterLists.map((value, index) => {
+              return (
+                    <tr>
+                      <td key={index}>{value.domain_name}</td>
+                      <td key={index}>{value.instance_id}</td>
+                      <td key={index}>{value.active_vehicle_count}</td>
+                      <td key={index}>{value.active_vehicle_count}</td>
+                    </tr>
+                )
+            })}
+          </tbody>
+        </Table>
+      </Card>
     )
   }
 }
-
 export default Cluster
