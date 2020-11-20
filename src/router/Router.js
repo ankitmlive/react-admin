57ea06e68294c59f56d0709b47e4db6f5546907c
ankitmlive <ankitmlive@gmail.com>
2020-11-20 17:10:09 +0530

    doc added

--- src/router/Router.js
+++ src/router/Router.js
@@ -42,6 +42,7 @@ const RouteConfig = ({ component: Component, fullLayout, ...rest }) => (
       }}
     />
   )
+  
 const mapStateToProps = state => {
     return {
       user: state.auth.login.userRole
