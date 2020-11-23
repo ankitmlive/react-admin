d7a953e78724bb2ed1879cf572448b74c49e9530
ankitmlive <ankitmlive@gmail.com>
2020-11-23 17:51:16 +0530

    mock api

--- src/@fake-db/mock.js
+++ src/@fake-db/mock.js
@@ -1,7 +1,14 @@
+// import MockAdapter from "axios-mock-adapter"
+// import axios from "axios"
+
+// const mock = new MockAdapter(axios)
+// //mock.onAny().passThrough()
+
+// export default mock
+
 import MockAdapter from "axios-mock-adapter"
 import axios from "axios"
 
 const mock = new MockAdapter(axios)
-mock.onAny().passThrough()
 
 export default mock
