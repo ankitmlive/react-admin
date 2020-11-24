2470ee64163f012a177d05da79b6201cd3892464
ankitmlive <ankitmlive@gmail.com>
2020-11-24 12:48:37 +0530

    mock conf

--- src/@fake-db/mock.js
+++ src/@fake-db/mock.js
@@ -1,14 +1,8 @@
-// import MockAdapter from "axios-mock-adapter"
-// import axios from "axios"
-
-// const mock = new MockAdapter(axios)
-// //mock.onAny().passThrough()
-
-// export default mock
-
 import MockAdapter from "axios-mock-adapter"
 import axios from "axios"
 
 const mock = new MockAdapter(axios)
+
+//mock.onAny().passThrough() its only for testing live api's
 
 export default mock
