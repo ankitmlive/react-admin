7e8908dc96c4291cae2dc504979110ec9beeff1d
ankitmlive <ankitmlive@gmail.com>
2020-12-01 17:22:13 +0530

    payment code

--- src/views/billing/payments.js
+++ src/views/billing/payments.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Row, Col } from "reactstrap"
 import Breadcrumbs from "../../components/@vuexy/breadCrumbs/BreadCrumb"
 import ListViewConfig from "./datalist"
+import PaymentViewConfig from "./paymentlist"
 import queryString from "query-string"
 
 class Payments extends React.Component {
@@ -13,6 +14,11 @@ class Payments extends React.Component {
           breadCrumbParent="Billing"
           breadCrumbActive="Payments"
         />
+        <Row>
+          <Col sm="12">
+            <PaymentViewConfig parsedFilter={queryString.parse(this.props.location.search)}/>
+          </Col>
+        </Row>
         <Row>
           <Col sm="12">
             <ListViewConfig parsedFilter={queryString.parse(this.props.location.search)}/>
